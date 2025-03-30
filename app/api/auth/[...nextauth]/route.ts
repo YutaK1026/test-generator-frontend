df52a20d6775626d6e454118f5548360a4fc37b1
YutaK1026 <inheritans1026@gmail.com>
2025-03-31 01:58:35 +0900

    deploy: secreteの文字列を修正

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -25,7 +25,7 @@ const authOptions: NextAuthOptions = {
       },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET
+  secret: "cZuHMOlmxMngBOgl/OyCyq+LqOUzLwi+3mJgz0BUDTU="
 };
 
 const handler = NextAuth(authOptions);
